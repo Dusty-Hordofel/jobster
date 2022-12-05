6fbbd345d1c45c26221c553eedceb7a3bffdfdf0
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-12-05 15:20:30 +0100

    23. Error Checking - Controller Example

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,17 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
-const register = async (req, res, next) => {
+const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+  });
 };
 
 const login = async (req, res, next) => {
