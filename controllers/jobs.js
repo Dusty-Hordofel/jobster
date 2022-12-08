a486f779e99f34140b3f8f8c236c0529966a269d
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-12-08 13:10:07 +0100

    52. Sort & 53. Pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -29,9 +29,37 @@ const getAllJobs = async (req, res, next) => {
   //get all job associated to the userId
   // const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
 
+  // NO AWAIT
   let result = Job.find(queryObject);
+
+  // chain sort conditions
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getAllUserJobs = async (req, res, next) => {
