7eba2dafe2c4aa06ceba06bb4986836616fc7e1a
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-12-08 23:05:31 +0100

    Update SharedLayout.js

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,26 +1,7 @@
 import React from "react";
 
-import { Outlet } from "react-router-dom";
-import { Navbar, SmallSidebar, BigSidebar } from "../../components";
-import Wrapper from "../../assets/wrappers/SharedLayout";
-
 const SharedLayout = () => {
-  return (
-    <>
-      <Wrapper>
-        <main className="dashboard">
-          <SmallSidebar />
-          <BigSidebar />
-          <div>
-            <Navbar />
-            <div className="dashboard-page">
-              <Outlet />
-            </div>
-          </div>
-        </main>
-      </Wrapper>
-    </>
-  );
+  return <div>SharedLayout</div>;
 };
 
 export default SharedLayout;
