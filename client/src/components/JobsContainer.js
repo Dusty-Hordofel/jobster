26bd1c16db815352a8afa0645951b1f1e182b27c
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-12-12 12:02:14 +0100

    107. Stat Item

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -1,10 +1,31 @@
 import { useEffect } from "react";
 import Job from "./Job";
-import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllJobs } from "../features/allJobs/allJobsSlice";
 import Loading from "./Loading";
+import styled from "styled-components";
 
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  h2 {
+    text-transform: none;
+  }
+  & > h5 {
+    font-weight: 700;
+  }
+  .jobs {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+  @media (min-width: 992px) {
+    .jobs {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
